f1013af0bf9a1e97b65f7e4eee2df10ed8912c50
Bohdan Oskin <bogdanosckinss@gmail.com>
2023-03-16 20:40:59 +0200

    Added conditions for rendering

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -15,7 +15,11 @@
                         <p style="margin-bottom: 10px">Колір</p>
                         <div style="display: flex; gap: 5px;">
                         @foreach($colors as $color)
-                            <a style="color: white; @if($color->color == "Green") background:green; @elseif($color->color == "Pink") background: deeppink; @elseif($color->color == "Black") background: black; @endif" class="product-model" href="{{route('product', $color->id)}}">{{$color->color}}</a>
+                            @if($color->amount > 0)
+                                <a style="color: white; @if($color->color == "Green") background:green; @elseif($color->color == "Pink") background: deeppink; @elseif($color->color == "Black") background: black; @endif @if($color->color == $product->color) border: solid black 3px; @endif" class="product-model" href="{{route('product', $color->id)}}">{{$color->color}}</a>
+                                @else
+                                <p style="color: white; @if($color->color == "Green") background:green; @elseif($color->color == "Pink") background: deeppink; @elseif($color->color == "Black") background: black; @endif @if($color->color == $product->color) border: solid black 3px; @endif" class="product-model">{{$color->color}}</p>
+                            @endif
                         @endforeach
                 </div>
                 </div>
@@ -26,7 +30,11 @@
                         <p style="margin-bottom: 10px">Модель</p>
                         <div style="display: flex; gap: 5px;">
                         @foreach($storage_gb as $storage_gbone)
-                            <a class="product-model" href="{{route('product', $storage_gbone->id)}}">{{$storage_gbone->storage_gb}}</a>
+                            @if($storage_gbone->amount > 0)
+                                <a class="product-model" style="@if($storage_gbone->storage_gb == $product->storage_gb) border: solid black 2px; @endif" href="{{route('product', $storage_gbone->id)}}">{{$storage_gbone->storage_gb}}</a>
+                                @else
+                                    <p class="product-model" style="background: #cbd5e0">{{$storage_gbone->storage_gb}}</p>
+                            @endif
                         @endforeach
                         </div>
                     </div>
