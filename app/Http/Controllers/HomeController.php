f28977e0b27d0cc08844bd23af646f87bab4847e
Bohdan Oskin <bogdanosckinss@gmail.com>
2023-03-16 22:21:33 +0200

    Optimized code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function home() {
-        $products = Product::where('amount', ">", 0)->get();
+        $category = Category::where('name', "Apple iPhone")->first();
 
-        return view('welcome', compact('products'));
+        return view('welcome', compact('category'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen">
 
             <div class="product-container" style="display: flex; gap: 10px; flex-wrap: wrap; width: 90vw; justify-content: center">
-                @isset($products)
-                    @foreach($products as $product)
+                @isset($category)
+                    @foreach($category->products as $product)
                        <div class="product">
                            <img src="{{asset('storage/images/'.$product->thumbnail)}}" alt="{{$product->category->name." ".$product->model." ".$product->storage_gb."GB ".$product->color}}">
                            <h2>{{$product->category->name}} {{$product->model}} {{$product->storage_gb}}GB {{$product->color}}</h2>
